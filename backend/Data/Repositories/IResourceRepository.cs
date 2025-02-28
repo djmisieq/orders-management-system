ec3d1c29a3c21592146eebb277812a1de9d8f1fc
djmisieq <zadroga.d@gmail.com>
2025-02-28 09:42:05 +0100

    Dodaj interfejs repozytorium zasobów

--- backend/Data/Repositories/IResourceRepository.cs
+++ backend/Data/Repositories/IResourceRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using OrdersManagement.Backend.Models;
@@ -10,7 +11,7 @@ namespace OrdersManagement.Backend.Data.Repositories
         Task<Resource> GetResourceByIdAsync(int id);
         Task<IEnumerable<Resource>> GetResourcesByTypeAsync(string resourceType);
         Task<IEnumerable<Resource>> GetResourcesByDepartmentAsync(string department);
-        Task<IEnumerable<Resource>> GetAvailableResourcesAsync(System.DateTime startTime, System.DateTime endTime);
+        Task<IEnumerable<Resource>> GetAvailableResourcesForPeriodAsync(DateTime start, DateTime end);
         Task<Resource> CreateResourceAsync(Resource resource);
         Task<Resource> UpdateResourceAsync(Resource resource);
         Task<bool> DeleteResourceAsync(int id);
