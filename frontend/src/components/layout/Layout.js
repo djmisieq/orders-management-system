cc8bb4b97e4a6b7973918aa52c718d8d8bf8243e
djmisieq <zadroga.d@gmail.com>
2025-02-28 09:26:27 +0100

    Zaktualizuj Layout z dodaniem nagłówka

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Header from './Header';
 import { Container } from 'react-bootstrap';
 import './Layout.css';
 
 const Layout = ({ children }) => {
   return (
-    <div className="layout-container">
-      <Sidebar />
-      <div className="content">
-        <Container fluid className="p-4">
-          {children}
-        </Container>
+    <div className="layout-wrapper">
+      <Header />
+      <div className="layout-container">
+        <Sidebar />
+        <div className="content">
+          <Container fluid className="p-4">
+            {children}
+          </Container>
+        </div>
       </div>
     </div>
   );
